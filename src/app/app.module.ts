2413cfca8a9b319946d7de886f744cc754754f43
clim877 <chamnap_lim@comcast.com>
2019-12-16 15:35:37 -0800

    Now, every request will be added authorization header bearer and token by TokenInterceptorService class.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,16 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { ReactiveFormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
+import { BrowserModule }                        from '@angular/platform-browser';
+import { NgModule }                             from '@angular/core';
+import { ReactiveFormsModule }                  from '@angular/forms';
+import { HttpClientModule, HTTP_INTERCEPTORS }  from '@angular/common/http';
+import { AppRoutingModule }                     from './app-routing.module';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { LoginComponent } from './components/login/login.component';
-import { SeatsComponent } from './components/seats/seats.component';
-import { HomeComponent } from './components/home/home.component';
+import { AppComponent }                         from './app.component';
+import { LoginComponent }                       from './components/login/login.component';
+import { SeatsComponent }                       from './components/seats/seats.component';
+import { HomeComponent }                        from './components/home/home.component';
+import { TokenInterceptorService }              from "./services/token-interceptor.service";
+import {AuthService} from "./services/auth.service";
+import {AuthGuard} from "./guards/auth.guard";
 
 @NgModule({
   declarations: [
@@ -22,7 +25,15 @@ import { HomeComponent } from './components/home/home.component';
     ReactiveFormsModule,
     HttpClientModule
   ],
-  providers: [],
+  providers: [
+    AuthService,
+    AuthGuard,
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: TokenInterceptorService,
+      multi: true
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
